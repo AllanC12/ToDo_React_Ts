206d0486cfb485afd4fc20f9bf2374510dca79e7
AllanC12 <allandevfront@gmail.com>
2024-01-10 22:41:54 -0300

    adicionando tasks no aray e definindo jsx de componente

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -12,7 +12,6 @@ interface Props {
 }
 
 
-
 const TaskForm = ({btnText,taskList,setTaskList}: Props) => {
 
   const [id,setId] = useState<number>(0)
@@ -23,12 +22,16 @@ const TaskForm = ({btnText,taskList,setTaskList}: Props) => {
     e.preventDefault()
 
     const id = Math.floor(Math.random() * 10000)
-    const newTask = {
+    const newTask: ITask = {
       id,
       title,
       difficulty
     }
 
+    setTaskList!([...taskList,newTask])
+
+    setTitle("")
+    setDifficulty(0)
   }
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -40,17 +43,15 @@ const TaskForm = ({btnText,taskList,setTaskList}: Props) => {
   }
 
 
-
-
   return (
     <form onSubmit={addTaskHandler}>
         <div className={styles.input_container}>
             <label htmlFor="title">Título:</label>
-            <input type="text" name="title" className={styles.input_txt} onChange={handleChange} placeholder="Título da sua tarefa" />
+            <input type="text" name="title" className={styles.input_txt} value={title} onChange={handleChange} placeholder="Título da sua tarefa" />
         </div>
         <div className={styles.input_container}>
             <label htmlFor="dificcult">Dificuldade:</label>
-            <input type="number" name="difficulty" className={styles.input_number} onChange={handleChange} placeholder="Dificuldade da tarefa"/>
+            <input type="number" name="difficulty" className={styles.input_number} value={difficulty} onChange={handleChange} placeholder="Dificuldade da tarefa"/>
         </div>
 
         <input type="submit" value={btnText} />
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,9 +1,31 @@
+import { ITask } from "../interfaces/Task"
 
+import styles from "./TaskList.module.css"
 
+interface Props {
+  taskList: ITask[]
+}
 
-const TaskList = () => {
+const TaskList = ({taskList}: Props) => {
   return (
-    <div>TaskList</div>
+    <div className={styles.taskList}>
+      {taskList.length > 0 ? (
+        <div>
+          {taskList.map(task => (
+            <div key={task.id}>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+              <div className={styles.icons}>
+                <i className="bi bi-pencil"></i>
+                <i className="bi bi-trash"></i>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Nao ha tarefas cadastradas</p>
+      )}
+    </div>
   )
 }
 
